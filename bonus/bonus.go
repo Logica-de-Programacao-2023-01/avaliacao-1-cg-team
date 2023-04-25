444edb4588524d8ea9dffb348c323422a1a966bb
Luiz Matheus Moraes Vega <luiz.mvega@dinfor.uniceub>
2023-04-25 10:58:19 -0300

    soluções_bonus

--- bonus/bonus.go
+++ bonus/bonus.go
@@ -1,5 +1,7 @@
 package bonus
 
+import "sort"
+
 //Joãozinho ganhou um kit de construção de torres. O kit é composto por várias barras de madeira, e seus comprimentos são
 //conhecidos. As barras podem ser empilhadas umas sobre as outras, desde que seus comprimentos sejam iguais.
 //
@@ -7,6 +9,21 @@ package bonus
 //barras da melhor maneira possível, determinando a altura da torre mais alta e quantas torres podem ser construídas.
 
 func CalculateTowers(barLengths []int) (int, int) {
-	// Seu código aqui
-	return 0, 0
+	if len(barLengths) < 1 {
+		return 0, 0
+	}
+	sort.Ints(barLengths)
+	maxHeight := 0
+	numTowers := 1
+	for i := 1; i < len(barLengths); i++ {
+		if barLengths[i] != barLengths[i-1] {
+
+			numTowers++
+		}
+		if barLengths[i] > maxHeight {
+			maxHeight = barLengths[i]
+		}
+	}
+	numTowers++
+	return numTowers, maxHeight
 }
